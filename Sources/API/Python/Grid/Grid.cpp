4671d317202d6643ec9ab57ab64b32202c490eb9
Chris Ohk <utilForever@gmail.com>
2018-10-06 23:57:46 +0900

    feat(python-api), style: Add properties and functions, format code style

--- Sources/API/Python/Grid/Grid.cpp
+++ Sources/API/Python/Grid/Grid.cpp
@@ -17,16 +17,14 @@ using namespace CubbyFlow;
 
 void AddGrid2(pybind11::module& m)
 {
-    pybind11::class_<Grid2, Grid2Ptr, Serializable>(
-        static_cast<pybind11::handle>(m), "Grid2",
-        R"pbdoc(
-			Abstract base class for 2-D cartesian grid structure.
-
-			This class represents 2-D cartesian grid structure. This class is an
-			abstract base class and does not store any data. The class only stores the
-			shape of the grid. The grid structure is axis-aligned and can have different
-			grid spacing per axis.
-		)pbdoc")
+    pybind11::class_<Grid2, Grid2Ptr, Serializable>(m, "Grid2",
+                                                    R"pbdoc(
+        Abstract base class for 2-D cartesian grid structure.
+        This class represents 2-D cartesian grid structure. This class is an
+        abstract base class and does not store any data. The class only stores the
+        shape of the grid. The grid structure is axis-aligned and can have different
+        grid spacing per axis.
+        )pbdoc")
         .def_property_readonly("resolution", &Grid2::GetResolution,
                                R"pbdoc(Resolution of the grid.)pbdoc")
         .def_property_readonly("origin", &Grid2::GetOrigin,
@@ -38,40 +36,43 @@ void AddGrid2(pybind11::module& m)
         .def_property_readonly(
             "cellCenterPosition", &Grid2::GetCellCenterPosition,
             R"pbdoc(Function that maps grid index to the cell-center position.)pbdoc")
-        .def(
-            "forEachCellIndex",
-            [](Grid2& instance, pybind11::function func) {
-                instance.ForEachCellIndex(func);
-            },
-            R"pbdoc(Invokes the given function `func` for each grid cell. This function invokes the given function object `func` for each grid
+        .def("forEachCellIndex",
+             [](Grid2& instance, pybind11::function func) {
+                 instance.ForEachCellIndex(func);
+             },
+             R"pbdoc(
+             Invokes the given function `func` for each grid cell.
+             This function invokes the given function object `func` for each grid
              cell in serial manner. The input parameters are `i` and `j` indices of a
-             grid cell. The order of execution is i-first, j-last.)pbdoc",
-            pybind11::arg("func"))
-        .def(
-            "hasSameShape",
-            [](const Grid2& instance, const Grid2Ptr& other) {
-                return instance.HasSameShape(*other);
-            },
-            R"pbdoc(Returns true if resolution, grid-spacing and origin are same.)pbdoc")
+             grid cell. The order of execution is i-first, j-last.
+             )pbdoc",
+             pybind11::arg("func"))
+        .def("hasSameShape",
+             [](const Grid2& instance, const Grid2Ptr& other) {
+                 return instance.HasSameShape(*other);
+             },
+             R"pbdoc(
+             Returns true if resolution, grid-spacing and origin are same.
+             )pbdoc")
         .def("swap",
              [](Grid2& instance, Grid2Ptr& other) {
                  return instance.Swap(other.get());
              },
-             R"pbdoc(Swaps the data with other grid.)pbdoc");
+             R"pbdoc(
+             Swaps the data with other grid.
+             )pbdoc");
 }
 
 void AddGrid3(pybind11::module& m)
 {
-    pybind11::class_<Grid3, Grid3Ptr, Serializable>(
-        static_cast<pybind11::handle>(m), "Grid3",
-        R"pbdoc(
-			Abstract base class for 3-D cartesian grid structure.
-
-			This class represents 3-D cartesian grid structure. This class is an
-			abstract base class and does not store any data. The class only stores the
-			shape of the grid. The grid structure is axis-aligned and can have different
-			grid spacing per axis.
-		)pbdoc")
+    pybind11::class_<Grid3, Grid3Ptr, Serializable>(m, "Grid3",
+                                                    R"pbdoc(
+        Abstract base class for 3-D cartesian grid structure.
+        This class represents 3-D cartesian grid structure. This class is an
+        abstract base class and does not store any data. The class only stores the
+        shape of the grid. The grid structure is axis-aligned and can have different
+        grid spacing per axis.
+        )pbdoc")
         .def_property_readonly("resolution", &Grid3::GetResolution,
                                R"pbdoc(Resolution of the grid.)pbdoc")
         .def_property_readonly("origin", &Grid3::GetOrigin,
@@ -83,24 +84,29 @@ void AddGrid3(pybind11::module& m)
         .def_property_readonly(
             "cellCenterPosition", &Grid3::GetCellCenterPosition,
             R"pbdoc(Function that maps grid index to the cell-center position.)pbdoc")
-        .def(
-            "forEachCellIndex",
-            [](Grid3& instance, pybind11::function func) {
-                instance.ForEachCellIndex(func);
-            },
-            R"pbdoc(Invokes the given function `func` for each grid cell. This function invokes the given function object `func` for each grid
-             cell in serial manner. The input parameters are `i`, `j` and `k` indices of a
-             grid cell. The order of execution is i-first, k-last.)pbdoc",
-            pybind11::arg("func"))
-        .def(
-            "hasSameShape",
-            [](const Grid3& instance, const Grid3Ptr& other) {
-                return instance.HasSameShape(*other);
-            },
-            R"pbdoc(Returns true if resolution, grid-spacing and origin are same.)pbdoc")
+        .def("forEachCellIndex",
+             [](Grid3& instance, pybind11::function func) {
+                 instance.ForEachCellIndex(func);
+             },
+             R"pbdoc(
+             Invokes the given function `func` for each grid cell.
+             This function invokes the given function object `func` for each grid
+             cell in serial manner. The input parameters are `i`, `j`, and `k` indices of a
+             grid cell. The order of execution is i-first, k-last.
+             )pbdoc",
+             pybind11::arg("func"))
+        .def("hasSameShape",
+             [](const Grid3& instance, const Grid3Ptr& other) {
+                 return instance.HasSameShape(*other);
+             },
+             R"pbdoc(
+             Returns true if resolution, grid-spacing and origin are same.
+             )pbdoc")
         .def("swap",
              [](Grid3& instance, Grid3Ptr& other) {
                  return instance.Swap(other.get());
              },
-             R"pbdoc(Swaps the data with other grid.)pbdoc");
+             R"pbdoc(
+             Swaps the data with other grid.
+             )pbdoc");
 }
--- Sources/API/Python/Size/Size.cpp
+++ Sources/API/Python/Size/Size.cpp
@@ -18,50 +18,52 @@ using namespace CubbyFlow;
 
 void AddSize2(pybind11::module& m)
 {
-	// NOTE: pybind11::module_local() is correct solution? :thinking:
-	pybind11::class_<Size2>(static_cast<pybind11::handle>(m), "Size2", pybind11::module_local())
-	.def("__init__", [](Size2& instance, size_t x, size_t y)
-	{
-		new (&instance) Size2(x, y);
-	},
-		R"pbdoc(
-			Constructs Size2
-
-			This method constructs 2D size with x and y.
-		)pbdoc",
-		pybind11::arg("x") = 0,
-		pybind11::arg("y") = 0)
-	.def_readwrite("x", &Size2::x)
-	.def_readwrite("y", &Size2::y)
-	.def("__eq__", [](const Size2& instance, pybind11::object obj)
-	{
-		Size2 other = ObjectToSize2(obj);
-		return instance == other;
-	});
+    pybind11::class_<Size2>(m, "Size2")
+        .def("__init__",
+             [](Size2& instance, size_t x, size_t y) {
+                 new (&instance) Size2(x, y);
+             },
+             R"pbdoc(
+             Constructs Size2
+             This method constructs 2D size with x and y.
+             )pbdoc",
+             pybind11::arg("x") = 0, pybind11::arg("y") = 0)
+        .def_readwrite("x", &Size2::x)
+        .def_readwrite("y", &Size2::y)
+        .def("__len__", [](const Size2&) { return 2; })
+        .def("__iter__",
+             [](const Size2& instance) {
+                 return pybind11::make_iterator(&instance.x, &instance.y + 1);
+             })
+        .def("__eq__", [](const Size2& instance, pybind11::object obj) {
+            Size2 other = ObjectToSize2(obj);
+            return instance == other;
+        });
 }
 
 void AddSize3(pybind11::module& m)
 {
-	// NOTE: pybind11::module_local() is correct solution? :thinking:
-	pybind11::class_<Size3>(static_cast<pybind11::handle>(m), "Size3", pybind11::module_local())
-	.def("__init__", [](Size3& instance, size_t x, size_t y, size_t z)
-	{
-		new (&instance) Size3(x, y, z);
-	},
-		R"pbdoc(
-			Constructs Size3
-
-			This method constructs 3D size with x, y, and z.
-		)pbdoc",
-		pybind11::arg("x") = 0,
-		pybind11::arg("y") = 0,
-		pybind11::arg("z") = 0)
-	.def_readwrite("x", &Size3::x)
-	.def_readwrite("y", &Size3::y)
-	.def_readwrite("z", &Size3::z)
-	.def("__eq__", [](const Size3& instance, pybind11::object obj)
-	{
-		Size3 other = ObjectToSize3(obj);
-		return instance == other;
-	});
+    pybind11::class_<Size3>(m, "Size3")
+        .def("__init__",
+             [](Size3& instance, size_t x, size_t y, size_t z) {
+                 new (&instance) Size3(x, y, z);
+             },
+             R"pbdoc(
+             Constructs Size3
+             This method constructs 3D size with x, y, and z.
+             )pbdoc",
+             pybind11::arg("x") = 0, pybind11::arg("y") = 0,
+             pybind11::arg("z") = 0)
+        .def_readwrite("x", &Size3::x)
+        .def_readwrite("y", &Size3::y)
+        .def_readwrite("z", &Size3::z)
+        .def("__len__", [](const Size3&) { return 3; })
+        .def("__iter__",
+             [](const Size3& instance) {
+                 return pybind11::make_iterator(&instance.x, &instance.z + 1);
+             })
+        .def("__eq__", [](const Size3& instance, pybind11::object obj) {
+            Size3 other = ObjectToSize3(obj);
+            return instance == other;
+        });
 }
